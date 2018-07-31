b22fd7e89ec2c2b023fae671364d1d09b6f06aa6
knd17u684 <kotovnd@mail.ru>
2018-07-31 03:04:32 +0300

    gunicorne added

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-sudo rm -r /etc/nginx/sites-enabled/default 
+sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo /etc/init.d/gunicorn restart
