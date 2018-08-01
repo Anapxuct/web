05adbeb27c6ee08f63eb1151b6dc7a54a64d04d0
Anapxuct <33698177+Anapxuct@users.noreply.github.com>
2018-08-01 03:09:58 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
-sudo rm -r /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# Nginx
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+touch /home/box/nginx.log
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# Gunicorn (ver. 17.5)
+touch /home/box/gunicorn.log
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
